fb4466a3225b84becac141cc25d23a57fc997c65
艾莉丝努力练剑 <18106882575@163.com>
2025-12-17 14:06:16 +0800

    12_17

--- 12_16/Test_12_16/myproc.c
+++ 12_16/Test_12_16/myproc.c
@@ -6,27 +6,31 @@ int g_val = 0;
 int main()
 {
     pid_t id = fork();
-    
-    if(id < 0)
+    int g_val = 0;
+
+    while(1)
     {
-        perror("fork");
-        return 0;
-    }
+        if(id < 0)
+        {
+            perror("fork");
+            return 0;
+        }
 
-    else if(id == 0)
-    { 
-        //child
-        printf("child[%d]: %d : %p\n", getpid(), g_val, &g_val);
-        sleep(1);
-    }
+        else if(id == 0)
+        { 
+            //child
+            printf("child[%d]: %d : %p\n", getpid(), g_val, &g_val);
+            g_val++;
+            sleep(1);
+        }
 
-    else
-    { 
-        //parent
-        printf("parent[%d]: %d : %p\n", getpid(), g_val, &g_val);
-        sleep(1);
+        else
+        { 
+            //parent
+            printf("parent[%d]: %d : %p\n", getpid(), g_val, &g_val);
+            sleep(1);
+        }
     }
     
     return 0;
 }
-
