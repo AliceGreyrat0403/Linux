30d4906eb7391822ee5afcb76cc98a24be0ef729
艾莉丝努力练剑 <18106882575@163.com>
2026-01-31 15:14:16 +0800

    1_31_IO && fd

---
+++ 1_31/mystdio/main.c
@@ -0,0 +1,6 @@
+#include "my_stdio.h"
+
+int main()
+{
+    return 0;
+}
---
+++ 1_31/mystdio/my_stdio.h
@@ -0,0 +1,11 @@
+#pragma once
+
+typedef struct{
+    int fd;
+    int flags;
+    int mode;   // 刷新策略
+    char outbuffer[1024];
+    //char inbuffer[1024];
+}My_FILE;
+
+My_FILE *Myopen(const char *pathname,const char *mode); // r w a方式
