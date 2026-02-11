1ac1992b5df2787ce234081cce8f8a6309d7de51
艾莉丝努力练剑 <18106882575@163.com>
2026-02-11 21:46:24 +0800

    old_centOS

---
+++ 12_8/code.c
@@ -0,0 +1,35 @@
+//#include <stdio.h>
+//#include <unistd.h>
+//
+//int main()
+//{
+//    int a = 10;
+//    scanf("%d", &a);
+//   // while(1)
+//   // {
+//   //     printf("我是一个进程, 我正在运行: %d？\n", getpid());
+//   // }
+//    return 0;
+//}
+
+//struct obj
+//{
+//    int a;
+//    int b;
+//    char c;
+//    double d;
+//};
+//
+//int main()
+//{
+//    struct obj x;
+//    printf("&x: %p, &(x.a): %p\n", &x, &(x.a));
+//    long long offset = (long long)&(((struct obj*)0)->d);
+//
+//    long long start = (long long)&(x.d) - offset;
+//    printf("offset: %d\n", offset);
+//
+//    printf("addr对比: %p, %p\n", &x, start);
+//
+//    return 0;
+//}
